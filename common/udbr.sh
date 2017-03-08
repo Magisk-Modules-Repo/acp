0fef4142185f07f6cfc80631e6e9133fc0269467
therealahrion <ahrionmgallegos@gmail.com>
2017-03-08 08:11:44 -0700

    deep buffer fixes and rm univ wipe on magisk

--- common/udbr.sh
+++ common/udbr.sh
@@ -2,41 +2,43 @@
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
-#### v THIS SHOULD MATCH YOUR CONFIG.SH MODID v #####
+## v THIS MATCHES YOUR CONFIG.SH MODID v ##
 MODID=udbr
-#### ^ THIS SHOULD MATCH YOUR CONFIG.SH MODID ^ #####
+## ^ THIS MATCHES YOUR CONFIG.SH MODID ^ ##
 
-################ v DO NOT REMOVE v ################
-AUDMODLIBPATH=/magisk/audmodlib
+rm -rf /cache/magisk/audmodlib
 
-SLOT=$(getprop ro.boot.slot_suffix 2>/tmp/null)
-if [ "$SLOT" ]; then
-  SYSTEM=/system/system
-else
-  SYSTEM=/system
-fi
+if [ ! -d /magisk/$MODID ]; then
+  ########## v DO NOT REMOVE v ##########
+  AUDMODLIBPATH=/magisk/audmodlib
 
-if [ ! -d "$SYSTEM/vendor" ] || [ -L "$SYSTEM/vendor" ]; then
-  VENDOR=/vendor
-elif [ -d "$SYSTEM/vendor" ] || [ -L "/vendor" ]; then
-  VENDOR=$SYSTEM/vendor
-fi
+  SLOT=$(getprop ro.boot.slot_suffix 2>/tmp/null)
+  if [ "$SLOT" ]; then
+    SYSTEM=/system/system
+  else
+    SYSTEM=/system
+  fi
 
-### FILE LOCATIONS ###
-# AUDIO EFFECTS
-CONFIG_FILE=$SYSTEM/etc/audio_effects.conf
-VENDOR_CONFIG=$VENDOR/etc/audio_effects.conf
-HTC_CONFIG_FILE=$SYSTEM/etc/htc_audio_effects.conf
-OTHER_VENDOR_FILE=$SYSTEM/etc/audio_effects_vendor.conf
-OFFLOAD_CONFIG=$SYSTEM/etc/audio_effects_offload.conf
-# AUDIO POLICY
-AUD_POL=$SYSTEM/etc/audio_policy.conf
-AUD_POL_CONF=$SYSTEM/etc/audio_policy_configuration.xml
-AUD_OUT_POL=$VENDOR/etc/audio_output_policy.conf
-V_AUD_POL=$VENDOR/etc/audio_policy.conf
-################ ^ DO NOT REMOVE ^ ################
+  if [ ! -d "$SYSTEM/vendor" ] || [ -L "$SYSTEM/vendor" ]; then
+    VENDOR=/vendor
+  elif [ -d "$SYSTEM/vendor" ] || [ -L "/vendor" ]; then
+    VENDOR=$SYSTEM/vendor
+  fi
 
-uninstall() {
+  ### FILE LOCATIONS ###
+  # AUDIO EFFECTS
+  CONFIG_FILE=$SYSTEM/etc/audio_effects.conf
+  VENDOR_CONFIG=$VENDOR/etc/audio_effects.conf
+  HTC_CONFIG_FILE=$SYSTEM/etc/htc_audio_effects.conf
+  OTHER_VENDOR_FILE=$SYSTEM/etc/audio_effects_vendor.conf
+  OFFLOAD_CONFIG=$SYSTEM/etc/audio_effects_offload.conf
+  # AUDIO POLICY
+  AUD_POL=$SYSTEM/etc/audio_policy.conf
+  AUD_POL_CONF=$SYSTEM/etc/audio_policy_configuration.xml
+  AUD_OUT_POL=$VENDOR/etc/audio_output_policy.conf
+  V_AUD_POL=$VENDOR/etc/audio_policy.conf
+  ########## v DO NOT REMOVE v ##########
+
   if [ -f $AUD_POL.bak ] || [ -f $AUD_POL_CONF.bak ] || [ -f $AUD_OUT_POL.bak ] || [ -f $V_AUD_POL.bak ]; then
     # RESTORE BACKED UP CONFIGS
     for RESTORE in $AUD_POL $AUD_POL_CONF $AUD_OUT_POL $V_AUD_POL; do
@@ -45,12 +47,7 @@ uninstall() {
       fi
     done
   fi
-}
 
-if [ ! -d /magisk/$MODID ]; then
-  uninstall
   rm -f /magisk/.core/post-fs-data.d/$MODID.sh
   reboot
 fi
-
-rm -rf /cache/magisk/audmodlib
