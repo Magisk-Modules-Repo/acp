83407a8e2e22107e74170e2c18a0e2cf9c1c07e3
Unknown <zackptg5@comcast.net>
2017-09-01 08:36:11 -0400

    Add autodetection of # of customrules, get rid of APPINSTALL variable

--- common/unity-uservariables.sh
+++ common/unity-uservariables.sh
@@ -2,14 +2,10 @@
 # User defined audmodlib variables
 # Add any variables here
 # CHANGE 'MODID' to whatever you want your mod folder to be in magisk (whatever you would normally place in config.sh)
-# CHANGE 'NUMOFCUSTRULES' to number of customrules files
-# CHANGE 'APPINSTALL' TO 'true' IF INCLUDING APKS
-# CHANGE 'NUMOFAPPS' to number of apps to install
+# CHANGE 'NUMOFAPPS' to number of apps to install (set to 0 if none)
 # For each app, have APP# set to the app name (have the apk contained in a folder with the same name too)
 # For example: APP1=ViPER4AndroidFX
 # ^ DO NOT MODIFY ^
 MODID=udbr
-NUMOFCUSTRULES=0
-APPINSTALL=false
 NUMOFAPPS=0
 APP1=
