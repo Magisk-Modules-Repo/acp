7295e3c78c6ad8f641c9c954e9c5c773386608f5
Zackptg5 <zackptg5@comcast.net>
2017-09-14 15:43:35 -0400

    Redo patching for xml

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -5,16 +5,24 @@
 # Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $AMLPATH$CONFIG_FILE
 # ^ DO NOT MODIFY ^
 if [ -f $V_AUD_OUT_POL ] && [ -f $AUD_POL_CONF ]; then
-  sed -i '/Speaker/{n;s/`#deep_buffer,`/deep_buffer,/;}' $AMLPATH$AUD_POL_CONF
-  sed -i '/Wired Headset/{n;s/`#deep_buffer,`/deep_buffer,/;}' $AMLPATH$AUD_POL_CONF
-  sed -i '/Wired Headphones/{n;s/`#deep_buffer,`/deep_buffer,/;}' $AMLPATH$AUD_POL_CONF
-elif [ ! -f $V_AUD_OUT_POL ] && [ -f $AUD_POL_CONF ]; then
-  sed -i 's/`#deep_buffer,`/deep_buffer,/g' $AMLPATH$AUD_POL_CONF
-  sed -i 's/`#,deep_buffer`/,deep_buffer/g' $AMLPATH$AUD_POL_CONF
+  for BUFFER in "Speaker" "Wired Headset" "Wired Headphones"; do
+    NUM=$(cat -n $AMLPATH$AUD_POL_CONF | sed -n "/$BUFFER/ {n;n;/deep_buffer,/p}" | sed 's/<!--.*//')
+	NUM=$((NUM-1))
+	sed -i "${NUM}d" $AMLPATH$AUD_POL_CONF
+    sed -ri "/$BUFFER/ {n;/deep_buffer,/ s/<!--(.*)-->/\1/g}" $AMLPATH$AUD_POL_CONF
+  done
+elif [ ! -f $V_AUD_OUT_POL ] && [ -f $AUD_POL_CONF ] && [ "$(grep "<!--.*deep_buffer" $AUD_POL_CONF)" ]; then
+  sed -ri -n '/( *)<!--(.*)deep_buffer/{x;d;};1h;1!{x;p;};${x;p;}' $AMLPATH$AUD_POL_CONF
+  sed -ri '/deep_buffer/ s/<!--(.*)-->/\1/g' $AMLPATH$AUD_POL_CONF
 else
-  for CFG in $A2DP_AUD_POL $AUD_POL $AUD_POL_CONF $AUD_POL_VOL $SUB_AUD_POL $USB_AUD_POL $V_AUD_OUT_POL $V_AUD_POL; do
-    if [ -f $CFG ]; then
+  for CFG in $AUD_POL $V_AUD_OUT_POL $V_AUD_POL; do
+    if [ -f $CFG ] && [ "$(grep '#deep_buffer' $AMLPATH$CFG)" ]; then
 	  sed -i '/deep_buffer {/,/}/ s/^#//' $AMLPATH$CFG
     fi
+  done
+  for CFG in $A2DP_AUD_POL $AUD_POL_CONF $AUD_POL_VOL $SUB_AUD_POL $USB_AUD_POL; do
+    if [ -f $CFG ] && [ "$(grep "<!--.*deep_buffer" $AMLPATH$CFG)" ]; then
+	  sed -i '/<!--deep_buffer {/,/}-->/ s/<!--deep_buffer/deep_buffer/g; s/}-->/}/g' $AMLPATH$CFG
+	fi
   done
 fi
--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -6,16 +6,27 @@
 # ^ DO NOT MODIFY ^
 ui_print "    Patching existing audio_policy files..."
 if [ -f $V_AUD_OUT_POL ] && [ -f $AUD_POL_CONF ]; then
-  sed -i '/Speaker/{n;s/deep_buffer,/`#deep_buffer,`/;}' $AMLPATH$AUD_POL_CONF
-  sed -i '/Wired Headset/{n;s/deep_buffer,/`#deep_buffer,`/;}' $AMLPATH$AUD_POL_CONF
-  sed -i '/Wired Headphones/{n;s/deep_buffer,/`#deep_buffer,`/;}' $AMLPATH$AUD_POL_CONF
-elif [ ! -f $V_AUD_OUT_POL ] && [ -f $AUD_POL_CONF ]; then
-  sed -i 's/deep_buffer,/`#deep_buffer,`/g' $AMLPATH$AUD_POL_CONF
-  sed -i 's/,deep_buffer/`#,deep_buffer`/g' $AMLPATH$AUD_POL_CONF
+  for BUFFER in "Speaker" "Wired Headset" "Wired Headphones"; do
+    if [ "$(sed -n "/$BUFFER/ {n;/deep_buffer,/ p}" $AMLPATH$AUD_POL_CONF)" ] && [ ! "$(sed -n "/$BUFFER/ {n;n;/deep_buffer,/p}" $AMLPATH$AUD_POL_CONF)" ]; then
+      sed -i "/$BUFFER/ {n;/deep_buffer,/ p}" $AMLPATH$AUD_POL_CONF
+      sed -ri "/$BUFFER/ {n;n;/deep_buffer,/ s/( *)(.*)/\1<!--\2-->/}" $AMLPATH$AUD_POL_CONF
+      sed -i "/$BUFFER/{n;s/deep_buffer,//;}" $AMLPATH$AUD_POL_CONF
+	fi
+  done
+elif [ ! -f $V_AUD_OUT_POL ] && [ -f $AUD_POL_CONF ] && [ "$(grep "deep_buffer," $AMLPATH$AUD_POL_CONF)" ] && [ ! "$(grep "<!--.*deep_buffer" $AMLPATH$AUD_POL_CONF)" ]; then
+  sed -ri "/(deep_buffer,|,deep_buffer)/p" $AMLPATH$AUD_POL_CONF
+  sed -ri '/(deep_buffer,|,deep_buffer)/{n;s/( *)(.*)deep_buffer(.*)/\1<!--\2deep_buffer\3-->/}' $AMLPATH$AUD_POL_CONF
+  sed -i '/<!--/!{/deep_buffer,/ s/deep_buffer,//g}' $AMLPATH$AUD_POL_CONF
+  sed -i '/<!--/!{/,deep_buffer/ s/,deep_buffer//g}' $AMLPATH$AUD_POL_CONF
 else
-  for CFG in $A2DP_AUD_POL $AUD_POL $AUD_POL_CONF $AUD_POL_VOL $SUB_AUD_POL $USB_AUD_POL $V_AUD_OUT_POL $V_AUD_POL; do
-    if [ -f $CFG ]; then
+  for CFG in $AUD_POL $V_AUD_OUT_POL $V_AUD_POL; do
+    if [ -f $CFG ] && [ ! "$(grep '#deep_buffer' $AMLPATH$CFG)" ] && [ "$(grep '^deep_buffer' $AMLPATH$CFG)" ]; then
 	  sed -i '/deep_buffer {/,/}/ s/^/#/' $AMLPATH$CFG
     fi
+  done
+  for CFG in $A2DP_AUD_POL $AUD_POL_CONF $AUD_POL_VOL $SUB_AUD_POL $USB_AUD_POL; do
+    if [ -f $CFG ] && [ ! "$(grep "<!--.*deep_buffer" $AMLPATH$CFG)" ]; then
+	  sed -i '/deep_buffer {/,/}/ s/deep_buffer/<!--deep_buffer/g; s/}/}-->/g' $AMLPATH$CFG
+	fi
   done
 fi
