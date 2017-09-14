1869066343c8332fd28d0c9373d5c1a5cd96ae22
Zackptg5 <zackptg5@comcast.net>
2017-09-13 21:52:58 -0400

    Have proper patching/removal of patches
    Inline comments are cool

--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -6,16 +6,16 @@
 # ^ DO NOT MODIFY ^
 ui_print "    Patching existing audio_policy files..."
 if [ -f $V_AUD_OUT_POL ] && [ -f $AUD_POL_CONF ]; then
-  sed -i '/Speaker/{n;s/deep_buffer,//;}' $AMLPATH$AUD_POL_CONF
-  sed -i '/Wired Headset/{n;s/deep_buffer,//;}' $AMLPATH$AUD_POL_CONF
-  sed -i '/Wired Headphones/{n;s/deep_buffer,//;}' $AMLPATH$AUD_POL_CONF
+  sed -i '/Speaker/{n;s/deep_buffer,/`#deep_buffer,`/;}' $AMLPATH$AUD_POL_CONF
+  sed -i '/Wired Headset/{n;s/deep_buffer,/`#deep_buffer,`/;}' $AMLPATH$AUD_POL_CONF
+  sed -i '/Wired Headphones/{n;s/deep_buffer,/`#deep_buffer,`/;}' $AMLPATH$AUD_POL_CONF
 elif [ ! -f $V_AUD_OUT_POL ] && [ -f $AUD_POL_CONF ]; then
-  sed -i 's/deep_buffer,//g' $AMLPATH$AUD_POL_CONF
-  sed -i 's/,deep_buffer//g' $AMLPATH$AUD_POL_CONF
+  sed -i 's/deep_buffer,/`#deep_buffer,`/g' $AMLPATH$AUD_POL_CONF
+  sed -i 's/,deep_buffer/`#,deep_buffer`/g' $AMLPATH$AUD_POL_CONF
 else
   for CFG in $A2DP_AUD_POL $AUD_POL $AUD_POL_CONF $AUD_POL_VOL $SUB_AUD_POL $USB_AUD_POL $V_AUD_OUT_POL $V_AUD_POL; do
     if [ -f $CFG ]; then
-	  sed -i '/deep_buffer {/,/}/d' $AMLPATH$CFG
+	  sed -i '/deep_buffer {/,/}/ s/^/#/' $AMLPATH$CFG
     fi
   done
 fi
