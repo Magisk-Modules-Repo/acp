601c07dde62aa35c730c20b7c515d08b2ffcddd2
Zackptg5 <zackptg5@comcast.net>
2017-10-15 12:54:41 -0400

    Fix permissions

--- config.sh
+++ config.sh
@@ -80,6 +80,11 @@ REPLACE="
 # NOTE: This part has to be adjusted to fit your own needs
 
 set_permissions() {
+  # DEFAULT PERMISSIONS, DON'T REMOVE THEM 
+  test "$MAGISK" == "true" && set_perm_recursive $MODPATH 0 0 0755 0644 
+ 
+  # CUSTOM PERMISSIONS
+  
   # Some templates if you have no idea what to do:
   # Note that all files/folders have the $UNITY prefix - keep this prefix on all of your files/folders
   # Also note the lack of '/' between variables - preceding slashes are already included in the variables
