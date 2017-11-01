10ceb63d1ceba01d22f94ceab4a1fe25030c0bbe
Zackptg5 <zackptg5@comcast.net>
2017-10-31 23:56:53 -0400

    Add prop removal with mod delete through magmanager

--- common/unity-audmodlib/audmodlib-post-fs-data.sh
+++ common/unity-audmodlib/audmodlib-post-fs-data.sh
@@ -52,6 +52,9 @@ set_sepolicy $SOURCE init unix_stream_socket connectto
 set_sepolicy $SOURCE property_socket sock_file getattr,open,read,write,execute
 set_sepolicy $SOURCE audio_prop
 
+# MAGISK PROP REMOVAL FUNCTION
+<PROPREMOVE>
+
 # MOD PATCHES
 
 for MOD in ${MODIDS}; do
