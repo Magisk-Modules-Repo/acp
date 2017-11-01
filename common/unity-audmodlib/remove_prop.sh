ee00a676b157401f6c372b44d90dd1a6f14319a8
Zackptg5 <zackptg5@comcast.net>
2017-11-01 00:03:44 -0400

    Missed something...

--- common/unity-audmodlib/remove_prop.sh
+++ common/unity-audmodlib/remove_prop.sh
@@ -1,11 +1,9 @@
 remove_prop() {
   AMLPROP="$AMLPATH/system.prop"
   if [ -f $AMLPROP ]; then
-    if [ "$(cat $1)" ]; then
-      cat $1 | while read PROP; do
-        test "$(grep "#$PROP" $AMLPROP)" && sed -i "\|#$PROP|d" $AMLPROP || { test "$(grep "$PROP" $AMLPROP)" && sed -i "\|$PROP|d" $AMLPROP; }
-      done
-    fi
+    cat $1 | while read PROP; do
+      test "$(grep "#$PROP" $AMLPROP)" && sed -i "\|#$PROP|d" $AMLPROP || { test "$(grep "$PROP" $AMLPROP)" && sed -i "\|$PROP|d" $AMLPROP; }
+    done
     test ! -s $AMLPROP && rm -f $AMLPROP
   fi
   rm -f $1
