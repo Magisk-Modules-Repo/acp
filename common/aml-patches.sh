808ddcf94afde8ca89492044486cd7f83675df7d
Zackptg5 <zackptg5@comcast.net>
2017-11-22 17:13:16 -0500

    vendor fix

--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -13,7 +13,7 @@ elif [ ! -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy.co
   sed -i '/<!--/!{/deep_buffer,/ s/deep_buffer,//g}' $AMLPATH$SYS/etc/audio_policy.conf
   sed -i '/<!--/!{/,deep_buffer/ s/,deep_buffer//g}' $AMLPATH$SYS/etc/audio_policy.conf
 else
-  for CFG in $SYS/etc/audio_policy.conf $VEN/etc/audio_output_policy.conf $VEN/etc/audio_policy.conf;; do
+  for CFG in $SYS/etc/audio_policy.conf $VEN/etc/audio_output_policy.conf $VEN/etc/audio_policy.conf; do
     if [ -f $CFG ] && [ ! "$(grep '#deep_buffer' $AMLPATH$CFG)" ] && [ "$(grep '^deep_buffer' $AMLPATH$CFG)" ]; then
 	  sed -i '/deep_buffer {/,/}/ s/^/#/' $AMLPATH$CFG
     fi
