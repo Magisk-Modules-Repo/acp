87ccf2969feffb8db98bd7a358bb803026fff898
Zackptg5 <zackptg5@comcast.net>
2017-11-22 19:51:00 -0500

    Replaced variables with wrong files before, whoops

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -1,15 +1,15 @@
-if [ -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy.conf ]; then
+if [ -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy_configuration.xml ]; then
   for BUFFER in "Speaker" "Wired Headset" "Wired Headphones"; do
-    NUM=$(cat -n $AMLPATH$SYS/etc/audio_policy.conf | sed -n "/$BUFFER/ {n;n;/deep_buffer,/p}" | sed 's/<!--.*//')
+    NUM=$(cat -n $AMLPATH$SYS/etc/audio_policy_configuration.xml | sed -n "/$BUFFER/ {n;n;/deep_buffer,/p}" | sed 's/<!--.*//')
 	NUM=$((NUM-1))
-	sed -i "${NUM}d" $AMLPATH$SYS/etc/audio_policy.conf
-    sed -ri "/$BUFFER/ {n;/deep_buffer,/ s/<!--(.*)-->/\1/g}" $AMLPATH$SYS/etc/audio_policy.conf
+	sed -i "${NUM}d" $AMLPATH$SYS/etc/audio_policy_configuration.xml
+    sed -ri "/$BUFFER/ {n;/deep_buffer,/ s/<!--(.*)-->/\1/g}" $AMLPATH$SYS/etc/audio_policy_configuration.xml
   done
-elif [ ! -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy.conf ] && [ "$(grep "<!--.*deep_buffer" $AMLPATH$SYS/etc/audio_policy.conf)" ]; then
-  sed -ri -n '/( *)<!--(.*)deep_buffer/{x;d;};1h;1!{x;p;};${x;p;}' $AMLPATH$SYS/etc/audio_policy.conf
-  sed -ri '/deep_buffer/ s/<!--(.*)-->/\1/g' $AMLPATH$SYS/etc/audio_policy.conf
+elif [ ! -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy_configuration.xml ] && [ "$(grep "<!--.*deep_buffer" $AMLPATH$SYS/etc/audio_policy_configuration.xml)" ]; then
+  sed -ri -n '/( *)<!--(.*)deep_buffer/{x;d;};1h;1!{x;p;};${x;p;}' $AMLPATH$SYS/etc/audio_policy_configuration.xml
+  sed -ri '/deep_buffer/ s/<!--(.*)-->/\1/g' $AMLPATH$SYS/etc/audio_policy_configuration.xml
 else
-  for CFG in $SYS/etc/audio_policy.conf $VEN/etc/audio_output_policy.conf $VEN/etc/audio_policy.conf; do
+  for CFG in $SYS/etc/audio_policy_configuration.xml $VEN/etc/audio_output_policy.conf $VEN/etc/audio_policy.conf; do
     if [ -f $CFG ] && [ "$(grep '#deep_buffer' $AMLPATH$CFG)" ]; then
 	  sed -i '/deep_buffer {/,/}/ s/^#//' $AMLPATH$CFG
     fi
--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -1,19 +1,19 @@
 ui_print "    Patching existing audio_policy files..."
-if [ -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy.conf ]; then
+if [ -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy_configuration.xml ]; then
   for BUFFER in "Speaker" "Wired Headset" "Wired Headphones"; do
-    if [ "$(sed -n "/$BUFFER/ {n;/deep_buffer,/ p}" $AMLPATH$SYS/etc/audio_policy.conf)" ] && [ ! "$(sed -n "/$BUFFER/ {n;n;/deep_buffer,/p}" $AMLPATH$SYS/etc/audio_policy.conf)" ]; then
-      sed -i "/$BUFFER/ {n;/deep_buffer,/ p}" $AMLPATH$SYS/etc/audio_policy.conf
-      sed -ri "/$BUFFER/ {n;n;/deep_buffer,/ s/( *)(.*)/\1<!--\2-->/}" $AMLPATH$SYS/etc/audio_policy.conf
-      sed -i "/$BUFFER/{n;s/deep_buffer,//;}" $AMLPATH$SYS/etc/audio_policy.conf
+    if [ "$(sed -n "/$BUFFER/ {n;/deep_buffer,/ p}" $AMLPATH$SYS/etc/audio_policy_configuration.xml)" ] && [ ! "$(sed -n "/$BUFFER/ {n;n;/deep_buffer,/p}" $AMLPATH$SYS/etc/audio_policy_configuration.xml)" ]; then
+      sed -i "/$BUFFER/ {n;/deep_buffer,/ p}" $AMLPATH$SYS/etc/audio_policy_configuration.xml
+      sed -ri "/$BUFFER/ {n;n;/deep_buffer,/ s/( *)(.*)/\1<!--\2-->/}" $AMLPATH$SYS/etc/audio_policy_configuration.xml
+      sed -i "/$BUFFER/{n;s/deep_buffer,//;}" $AMLPATH$SYS/etc/audio_policy_configuration.xml
 	fi
   done
-elif [ ! -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy.conf ] && [ "$(grep "deep_buffer," $AMLPATH$SYS/etc/audio_policy.conf)" ] && [ ! "$(grep "<!--.*deep_buffer" $AMLPATH$SYS/etc/audio_policy.conf)" ]; then
-  sed -ri "/(deep_buffer,|,deep_buffer)/p" $AMLPATH$SYS/etc/audio_policy.conf
-  sed -ri '/(deep_buffer,|,deep_buffer)/{n;s/( *)(.*)deep_buffer(.*)/\1<!--\2deep_buffer\3-->/}' $AMLPATH$SYS/etc/audio_policy.conf
-  sed -i '/<!--/!{/deep_buffer,/ s/deep_buffer,//g}' $AMLPATH$SYS/etc/audio_policy.conf
-  sed -i '/<!--/!{/,deep_buffer/ s/,deep_buffer//g}' $AMLPATH$SYS/etc/audio_policy.conf
+elif [ ! -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy_configuration.xml ] && [ "$(grep "deep_buffer," $AMLPATH$SYS/etc/audio_policy_configuration.xml)" ] && [ ! "$(grep "<!--.*deep_buffer" $AMLPATH$SYS/etc/audio_policy_configuration.xml)" ]; then
+  sed -ri "/(deep_buffer,|,deep_buffer)/p" $AMLPATH$SYS/etc/audio_policy_configuration.xml
+  sed -ri '/(deep_buffer,|,deep_buffer)/{n;s/( *)(.*)deep_buffer(.*)/\1<!--\2deep_buffer\3-->/}' $AMLPATH$SYS/etc/audio_policy_configuration.xml
+  sed -i '/<!--/!{/deep_buffer,/ s/deep_buffer,//g}' $AMLPATH$SYS/etc/audio_policy_configuration.xml
+  sed -i '/<!--/!{/,deep_buffer/ s/,deep_buffer//g}' $AMLPATH$SYS/etc/audio_policy_configuration.xml
 else
-  for CFG in $SYS/etc/audio_policy.conf $VEN/etc/audio_output_policy.conf $VEN/etc/audio_policy.conf; do
+  for CFG in $SYS/etc/audio_policy_configuration.xml $VEN/etc/audio_output_policy.conf $VEN/etc/audio_policy.conf; do
     if [ -f $CFG ] && [ ! "$(grep '#deep_buffer' $AMLPATH$CFG)" ] && [ "$(grep '^deep_buffer' $AMLPATH$CFG)" ]; then
 	  sed -i '/deep_buffer {/,/}/ s/^/#/' $AMLPATH$CFG
     fi
