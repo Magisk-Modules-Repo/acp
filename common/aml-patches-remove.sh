fc9817de05ddf5a2ed1662bceea892f9d8709693
Zackptg5 <zackptg5@comcast.net>
2017-11-29 15:40:27 -0500

    Fix bugs

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -9,13 +9,10 @@ elif [ ! -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy_co
   sed -ri -n '/( *)<!--(.*)deep_buffer/{x;d;};1h;1!{x;p;};${x;p;}' $AMLPATH$SYS/etc/audio_policy_configuration.xml
   sed -ri '/deep_buffer/ s/<!--(.*)-->/\1/g' $AMLPATH$SYS/etc/audio_policy_configuration.xml
 else
-  for CFG in $SYS/etc/audio_policy_configuration.xml $VEN/etc/audio_output_policy.conf $VEN/etc/audio_policy.conf; do
-    if [ -f $CFG ] && [ "$(grep '#deep_buffer' $AMLPATH$CFG)" ]; then
-	  sed -i '/deep_buffer {/,/}/ s/^#//' $AMLPATH$CFG
-    fi
-  done
   for FILE in ${POLS}; do
-    if [ "$(grep "<!--.*deep_buffer" $AMLPATH$FILE)" ]; then
+    if [ "$FILE" == "*.conf" -o "$FILE" == "$SYS/etc/audio_policy_configuration.xml" ] && [ ! "$(grep "#deep_buffer" $AMLPATH$FILE)" ] && [ "$(grep '^deep_buffer' $AMLPATH$FILE)" ]; then
+	  sed -i '/deep_buffer {/,/}/ s/^#//' $AMLPATH$FILE
+	elif [ "$FILE" == "*.xml" ] && [ "$(grep "<!--.*deep_buffer" $AMLPATH$FILE)" ]; then
 	  sed -i '/<!--deep_buffer {/,/}-->/ s/<!--deep_buffer/deep_buffer/g; s/}-->/}/g' $AMLPATH$FILE
 	fi
   done
