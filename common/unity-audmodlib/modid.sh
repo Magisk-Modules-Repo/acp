938144392189478efc41ece34eeca28b04a728f3
Zackptg5 <zackptg5@comcast.net>
2017-12-06 13:12:46 -0500

    Remove oreonew stuff - no need for it

--- common/unity-audmodlib/modid.sh
+++ common/unity-audmodlib/modid.sh
@@ -9,8 +9,6 @@ ROOT=<ROOT>
 SYS=<SYS>
 VEN=<VEN>
 SOURCE=<SOURCE>
-LIBDIR=<LIBDIR>
-LIBPATCH=<LIBPATCH>
 if [ -d $SYS/priv-app ]; then SOURCE=priv_app; else SOURCE=system_app; fi
 
 ### FILE LOCATIONS ###
