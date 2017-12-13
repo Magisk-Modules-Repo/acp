a446c3c55f47091fb4ebcacd5b1572c02cf43957
Zackptg5 <zackptg5@comcast.net>
2017-12-13 16:11:15 -0500

    Set selinux to permissive for OOS oreo, improve sepolicy patching

--- common/unity-audmodlib/audmodlib-post-fs-data.sh
+++ common/unity-audmodlib/audmodlib-post-fs-data.sh
@@ -11,13 +11,15 @@ MODIDS=""
 LIBDIR=<LIBDIR>
 ### FILE LOCATIONS ###
 CFGS="${CFGS} $(find -L $SYS -type f -name "*audio_effects*.conf")"
-POLS="${POLS} $(find -L $SYS -type f -name "*audio*policy*.conf" -o -name "*audio_policy*.xml")"
+CFGSXML="${CFGSXML} $(find -L $SYS -type f -name "*audio_effects*.xml")"
+POLS="${POLS} $(find -L $SYS -type f -name "*audio*policy*.conf")"
+POLSXML="${POLSXML} $(find -L $SYS -type f -name "*audio_policy*.xml")"
 MIXS="${MIXS} $(find -L $SYS -type f -name "*mixer_paths*.xml")"
 
 # SEPOLICY SETTING FUNCTION
 set_sepolicy() {
   if [ "$(basename $SEINJECT)" == "sepolicy-inject" ]; then
-	  if [ -z $2 ]; then $SEINJECT -Z $1 -l; else $SEINJECT -s $1 -t $2 -c $3 -p $4 -l; fi
+	  if [ -z $2 ]; then $SEINJECT -Z $(echo $1 | sed 's/,/; set_sepolicy /g') -l; else $SEINJECT -s $1 -t $2 -c $3 -p $4 -l; fi
   else
     if [ -z $2 ]; then $SEINJECT --live "permissive $(echo $1 | sed 's/,/ /g')"; else $SEINJECT --live "allow $1 $2 $3 { $(echo $4 | sed 's/,/ /g') }"; fi
   fi
@@ -31,8 +33,7 @@ set_sepolicy mediaserver mediaserver_tmpfs file read,write,execute
 set_sepolicy mediaserver system_file file execmod
 set_sepolicy $SOURCE init unix_stream_socket connectto
 set_sepolicy $SOURCE property_socket sock_file getattr,open,read,write,execute
-set_sepolicy $SOURCE
-set_sepolicy audio_prop
+set_sepolicy $SOURCE,audio_prop
 
 # MOD PATCHES
 
--- common/unity-audmodlib/modid.sh
+++ common/unity-audmodlib/modid.sh
@@ -13,13 +13,15 @@ if [ -d $SYS/priv-app ]; then SOURCE=priv_app; else SOURCE=system_app; fi
 
 ### FILE LOCATIONS ###
 CFGS="${CFGS} $(find -L $SYS -type f -name "*audio_effects*.conf")"
-POLS="${POLS} $(find -L $SYS -type f -name "*audio*policy*.conf" -o -name "*audio_policy*.xml")"
+CFGSXML="${CFGSXML} $(find -L $SYS -type f -name "*audio_effects*.xml")"
+POLS="${POLS} $(find -L $SYS -type f -name "*audio*policy*.conf")"
+POLSXML="${POLSXML} $(find -L $SYS -type f -name "*audio_policy*.xml")"
 MIXS="${MIXS} $(find -L $SYS -type f -name "*mixer_paths*.xml")"
 
 # SEPOLICY SETTING FUNCTION
 set_sepolicy() {
   if [ "$(basename $SEINJECT)" == "sepolicy-inject" ]; then
-	  if [ -z $2 ]; then $SEINJECT -Z $1 -l; else $SEINJECT -s $1 -t $2 -c $3 -p $4 -l; fi
+	  if [ -z $2 ]; then $SEINJECT -Z $(echo $1 | sed 's/,/; set_sepolicy /g') -l; else $SEINJECT -s $1 -t $2 -c $3 -p $4 -l; fi
   else
     if [ -z $2 ]; then $SEINJECT --live "permissive $(echo $1 | sed 's/,/ /g')"; else $SEINJECT --live "allow $1 $2 $3 { $(echo $4 | sed 's/,/ /g') }"; fi
   fi
