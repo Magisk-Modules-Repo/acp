259626de4de66775fdb1f3f29eba5b03475ed649
Zackptg5 <zackptg5@comcast.net>
2017-12-20 19:08:35 -0500

    Script bug fix, separate sepolicy patches to each module

--- common/unity-audmodlib/audmodlib-post-fs-data.sh
+++ common/unity-audmodlib/audmodlib-post-fs-data.sh
@@ -7,41 +7,19 @@ ROOT=<ROOT>
 SYS=<SYS>
 VEN=<VEN>
 SOURCE=<SOURCE>
-MODIDS=""
 LIBDIR=<LIBDIR>
+MODIDS=""
 ### FILE LOCATIONS ###
-CFGS="${CFGS} $(find -L $SYS -type f -name "*audio_effects*.conf")"
-CFGSXML="${CFGSXML} $(find -L $SYS -type f -name "*audio_effects*.xml")"
-POLS="${POLS} $(find -L $SYS -type f -name "*audio*policy*.conf")"
-POLSXML="${POLSXML} $(find -L $SYS -type f -name "*audio_policy*.xml")"
-MIXS="${MIXS} $(find -L $SYS -type f -name "*mixer_paths*.xml")"
-
-# SEPOLICY SETTING FUNCTION
-set_sepolicy() {
-  if [ "$(basename $SEINJECT)" == "sepolicy-inject" ]; then
-	  if [ -z $2 ]; then $SEINJECT -Z $(echo $1 | sed 's/,/; set_sepolicy /g') -l; else $SEINJECT -s $1 -t $2 -c $3 -p $4 -l; fi
-  else
-    if [ -z $2 ]; then $SEINJECT --live "permissive $(echo $1 | sed 's/,/ /g')"; else $SEINJECT --live "allow $1 $2 $3 { $(echo $4 | sed 's/,/ /g') }"; fi
-  fi
-}
-
-set_sepolicy hal_audio_default audio_data_file dir search
-set_sepolicy hal_audio_default hal_audio_default_tmpfs file execute
-set_sepolicy hal_audio_default hal_audio_default process execmem
-set_sepolicy audioserver unlabeled file read,write,open,getattr,execute
-set_sepolicy audioserver audioserver_tmpfs file read,write,execute
-set_sepolicy audioserver system_file file execmod
-set_sepolicy mediaserver mediaserver_tmpfs file read,write,execute
-set_sepolicy mediaserver system_file file execmod
-set_sepolicy $SOURCE init unix_stream_socket connectto
-set_sepolicy $SOURCE property_socket sock_file getattr,open,read,write,execute
-set_sepolicy $SOURCE,audio_prop
-
+CFGS="<CFGS>"
+CFGSXML="<CFGSXML>"
+POLS="<POLS>"
+POLSXML="<POLSXML>"
+MIXS="<MIXS>"
 # MOD PATCHES
 
 if [ "$MAGISK" == true ]; then
   for MOD in ${MODIDS}; do
-    sed -i "/^#$MODID/,/fi #$MODID/d" $SH/post-fs-data.sh
+    sed -i "/^#$MOD/,/fi #$MOD/d" $SH/post-fs-data.sh
   done
   test ! "$(sed -n '/# MOD PATCHES/{n;p}' $AMLPATH/post-fs-data.sh)" && rm -rf $AMLPATH
 fi
--- common/unity-audmodlib/modid.sh
+++ common/unity-audmodlib/modid.sh
@@ -1,6 +1,5 @@
 <SHEBANG>
 SH=${0%/*}
-MODID=<MODID>
 SEINJECT=<SEINJECT>
 AMLPATH=<AMLPATH>
 MAGISK=<MAGISK>
@@ -9,19 +8,17 @@ SYS=<SYS>
 VEN=<VEN>
 SOURCE=<SOURCE>
 LIBDIR=<LIBDIR>
-if [ -d $SYS/priv-app ]; then SOURCE=priv_app; else SOURCE=system_app; fi
-
+MODID=<MODID>
 ### FILE LOCATIONS ###
-CFGS="${CFGS} $(find -L $SYS -type f -name "*audio_effects*.conf")"
-CFGSXML="${CFGSXML} $(find -L $SYS -type f -name "*audio_effects*.xml")"
-POLS="${POLS} $(find -L $SYS -type f -name "*audio*policy*.conf")"
-POLSXML="${POLSXML} $(find -L $SYS -type f -name "*audio_policy*.xml")"
-MIXS="${MIXS} $(find -L $SYS -type f -name "*mixer_paths*.xml")"
-
+CFGS="<CFGS>"
+CFGSXML="<CFGSXML>"
+POLS="<POLS>"
+POLSXML="<POLSXML>"
+MIXS="<MIXS>"
 # SEPOLICY SETTING FUNCTION
 set_sepolicy() {
   if [ "$(basename $SEINJECT)" == "sepolicy-inject" ]; then
-	  if [ -z $2 ]; then $SEINJECT -Z $(echo $1 | sed 's/,/; set_sepolicy /g') -l; else $SEINJECT -s $1 -t $2 -c $3 -p $4 -l; fi
+	  if [ -z $2 ]; then $SEINJECT -Z $1 -l; else $SEINJECT -s $1 -t $2 -c $3 -p $4 -l; fi
   else
     if [ -z $2 ]; then $SEINJECT --live "permissive $(echo $1 | sed 's/,/ /g')"; else $SEINJECT --live "allow $1 $2 $3 { $(echo $4 | sed 's/,/ /g') }"; fi
   fi
