e2c54f2baca3ecc0b96b4517570e6a52e1b50fc1
Zackptg5 <zackptg5@comcast.net>
2018-03-03 13:43:56 -0500

    Unity already takes care of this...

--- common/install.sh
+++ common/install.sh
@@ -1,10 +1,6 @@
 ui_print "   Patching existing audio_policy files..."
 if [ -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy_configuration.xml ]; then
-  if $MAGISK; then
-    cp_ch $ORIGDIR$SYS/etc/audio_policy_configuration.xml $UNITY$SYS/etc/audio_policy_configuration.xml
-  else
-    [ ! -f $ORIGDIR$SYS/etc/audio_policy_configuration.xml.bak ] && cp_ch $ORIGDIR$SYS/etc/audio_policy_configuration.xml $UNITY$SYS/etc/audio_policy_configuration.xml.bak
-  fi
+  cp_ch $ORIGDIR$SYS/etc/audio_policy_configuration.xml $UNITY$SYS/etc/audio_policy_configuration.xml
   for BUFFER in "Earpiece" "Speaker" "Wired Headset" "Wired Headphones" "Line" "HDMI" "Proxy" "FM" "BT SCO All" "USB Device Out" "Telephony Tx" "voice_rx" "primary input" "surround_sound" "record_24" "BT A2DP Out" "BT A2DP Headphones" "BT A2DP Speaker"; do
     if [ "$(sed -n "/$BUFFER/ {n;/deep_buffer,/ p}" $UNITY$SYS/etc/audio_policy_configuration.xml)" ] && [ ! "$(sed -n "/$BUFFER/ {n;n;/deep_buffer,/p}" $UNITY$SYS/etc/audio_policy_configuration.xml)" ]; then
       sed -i "/$BUFFER/ {n;/deep_buffer,/ p}" $UNITY$SYS/etc/audio_policy_configuration.xml
@@ -13,32 +9,20 @@ if [ -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy_config
     fi
   done
 elif [ ! -f $VEN/etc/audio_output_policy.conf ] && [ -f $SYS/etc/audio_policy_configuration.xml ]; then
-  if $MAGISK; then
-    cp_ch $ORIGDIR$SYS/etc/audio_policy_configuration.xml $UNITY$SYS/etc/audio_policy_configuration.xml
-  else
-    [ ! -f $ORIGDIR$SYS/etc/audio_policy_configuration.xml.bak ] && cp_ch $ORIGDIR$SYS/etc/audio_policy_configuration.xml $UNITY$SYS/etc/audio_policy_configuration.xml.bak
-  fi
+  cp_ch $ORIGDIR$SYS/etc/audio_policy_configuration.xml $UNITY$SYS/etc/audio_policy_configuration.xml
   sed -ri "/(deep_buffer,|,deep_buffer)/p" $UNITY$SYS/etc/audio_policy_configuration.xml
   sed -ri "/(deep_buffer,|,deep_buffer)/{n;s/( *)(.*)deep_buffer(.*)/\1<!--\2deep_buffer\3-->/}" $UNITY$SYS/etc/audio_policy_configuration.xml
   sed -i "/<!--/!{/deep_buffer,/ s/deep_buffer,//g}" $UNITY$SYS/etc/audio_policy_configuration.xml
   sed -i "/<!--/!{/,deep_buffer/ s/,deep_buffer//g}" $UNITY$SYS/etc/audio_policy_configuration.xml
 elif [ -f $VEN/etc/audio/audio_policy_configuration.xml ]; then
-  if $MAGISK; then
-    cp_ch $ORIGDIR$VEN/etc/audio/audio_policy_configuration.xml $UNITY$VEN/etc/audio/audio_policy_configuration.xml
-  else
-    [ ! -f $ORIGDIR$VEN/etc/audio/audio_policy_configuration.xml.bak ] && cp_ch $ORIGDIR$VEN/etc/audio/audio_policy_configuration.xml $UNITY$VEN/etc/audio/audio_policy_configuration.xml.bak
-  fi
+  cp_ch $ORIGDIR$VEN/etc/audio/audio_policy_configuration.xml $UNITY$VEN/etc/audio/audio_policy_configuration.xml
   sed -ri "/(deep_buffer,|,deep_buffer)/p" $UNITY$VEN/etc/audio/audio_policy_configuration.xml
   sed -ri "/(deep_buffer,|,deep_buffer)/{n;s/( *)(.*)deep_buffer(.*)/\1<!--\2deep_buffer\3-->/}" $UNITY$VEN/etc/audio/audio_policy_configuration.xml
   sed -i "/<!--/!{/deep_buffer,/ s/deep_buffer,//g}" $UNITY$VEN/etc/audio/audio_policy_configuration.xml
   sed -i "/<!--/!{/,deep_buffer/ s/,deep_buffer//g}" $UNITY$VEN/etc/audio/audio_policy_configuration.xml
 else
   for FILE in ${POLS}; do
-    if $MAGISK; then
-      cp_ch $ORIGDIR$FILE $UNITY$FILE
-    else
-      [ ! -f $ORIGDIR$FILE.bak ] && cp_ch $ORIGDIR$FILE $UNITY$FILE.bak
-    fi
+    cp_ch $ORIGDIR$FILE $UNITY$FILE
     $MAGISK && cp_ch $ORIGDIR$FILE $UNITY$FILE
     case $FILE in
       *.conf) if [ ! "$(grep "# *deep_buffer" $UNITY$FILE)" ] && [ "$(grep '^ *deep_buffer' $UNITY$FILE)" ]; then
