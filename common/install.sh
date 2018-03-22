8a5d6eade64eca2e8d8a53505604378a556bc008
Zackptg5 <zackptg5@comcast.net>
2018-03-22 08:04:02 -0400

    duh

--- common/install.sh
+++ common/install.sh
@@ -1,7 +1,7 @@
 ui_print "   Patching existing audio policy files..."
 for FILE in ${POLS}; do
+  cp_ch $ORIGDIR$FILE $UNITY$FILE
   case $FILE in
-    cp_ch $ORIGDIR$FILE $UNITY$FILE
     *.xml) sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort> *$/ {/flags=\".*\">$/p; s|( *)(.*flags=\".*\">$)|\1<!--\2$MODID-->|}" $UNITY$FILE
           sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort> *$/ {/<!--/! {s|flags=\".*\"|flags=\"AUDIO_OUTPUT_FLAG_FAST\"|}}" $UNITY$FILE;;
     *.conf) sed -ri "/^ *(deep_buffer)|(raw) \{/,/}/ {/flags .*$/p; s|( *flags .*$)|#$MODID\1|}" $UNITY$FILE
