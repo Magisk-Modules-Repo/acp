18c9c3333bc042990e3b5cb3c2c4b51ea9a8bba7
Zackptg5 <zackptg5@comcast.net>
2018-03-22 07:56:43 -0400

    more fixes

--- common/install.sh
+++ common/install.sh
@@ -2,9 +2,9 @@ ui_print "   Patching existing audio policy files..."
 for FILE in ${POLS}; do
   case $FILE in
     cp_ch $ORIGDIR$FILE $UNITY$FILE
-    *xml) sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort> *$/ {/flags=\".*\">$/p; s|( *)(.*flags=\".*\">$)|\1<!--\2$MODID-->|}" $UNITY$FILE
+    *.xml) sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort> *$/ {/flags=\".*\">$/p; s|( *)(.*flags=\".*\">$)|\1<!--\2$MODID-->|}" $UNITY$FILE
           sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort> *$/ {/<!--/! {s|flags=\".*\"|flags=\"AUDIO_OUTPUT_FLAG_FAST\"|}}" $UNITY$FILE;;
-    *conf) sed -ri "/^ *(deep_buffer)|(raw) \{/,/}/ {/flags .*$/p; s|( *flags .*$)|#$MODID\1|}" $UNITY$FILE
+    *.conf) sed -ri "/^ *(deep_buffer)|(raw) \{/,/}/ {/flags .*$/p; s|( *flags .*$)|#$MODID\1|}" $UNITY$FILE
            sed -ri "/^ *(deep_buffer)|(raw) \{/,/}/ {/^ *flags .*$/ s|(flags .*)|flags AUDIO_OUTPUT_FLAG_FAST|}" $UNITY$FILE;;
   esac
 done
--- common/uninstall.sh
+++ common/uninstall.sh
@@ -1,6 +1,6 @@
 $MAGISK || { for FILE in ${POLS}; do
   case $FILE in
-    *xml) sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort>/ {/flags=\"AUDIO_OUTPUT_FLAG_FAST.*\">$/d; s|( *)<!--(.*flags=\".*\".*)$MODID-->|\1\2|}" $UNITY$FILE;;
-    *conf) sed -ri "/^ *(deep_buffer)|(raw) \{/,/}/ {/^ *flags AUDIO_OUTPUT_FLAG_FAST$/d; s|#$MODID||}" $UNITY$FILE;;
+    *.xml) sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort>/ {/flags=\"AUDIO_OUTPUT_FLAG_FAST.*\">$/d; s|( *)<!--(.*flags=\".*\".*)$MODID-->|\1\2|}" $UNITY$FILE;;
+    *.conf) sed -ri "/^ *(deep_buffer)|(raw) \{/,/}/ {/^ *flags AUDIO_OUTPUT_FLAG_FAST$/d; s|#$MODID||}" $UNITY$FILE;;
   esac
 done }
