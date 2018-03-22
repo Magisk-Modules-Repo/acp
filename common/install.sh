922943d94dd1868be39f35922c112480df1fe84f
Zackptg5 <zackptg5@comcast.net>
2018-03-21 20:38:20 -0400

    Add modid id, fine tune

--- common/install.sh
+++ common/install.sh
@@ -3,11 +3,11 @@ for FILE in ${POLS}; do
   if [ ! -z $XML ]; then
     [ "$(basename $FILE)" != "audio_policy_configuration.xml" ] && continue
     cp_ch $ORIGDIR$FILE $UNITY$FILE
-    sed -ri '/<mixPort name="(deep_buffer)|(raw)"/,/<\/mixPort>/ {/flags=".*"/p; s|( *)(.*flags=".*".*>)|\1<!--\2-->|}' $UNITY$FILE
-    sed -ri '/<mixPort name="(deep_buffer)|(raw)"/,/<\/mixPort>/ {/<!--/! {s|flags=".*"|flags="AUDIO_OUTPUT_FLAG_FAST"|}}' $UNITY$FILE
+    sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort> *$/ {/flags=\".*\">$/p; s|( *)(.*flags=\".*\">$)|\1<!--\2$MODID-->|}" $UNITY$FILE
+    sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort> *$/ {/<!--/! {s|flags=\".*\"|flags=\"AUDIO_OUTPUT_FLAG_FAST\"|}}" $UNITY$FILE
   else
     cp_ch $ORIGDIR$FILE $UNITY$FILE
-    sed -i "/^ *deep_buffer {/,/}/ s/^/#/" $UNITY$FILE
-    sed -i "/^ *raw {/,/}/ s/^/#/" $UNITY$FILE
+    sed -i "/^ *deep_buffer {/,/}/ s/^/#$MODID/" $UNITY$FILE
+    sed -i "/^ *raw {/,/}/ s/^/#$MODID/" $UNITY$FILE
   fi
 done
--- common/uninstall.sh
+++ common/uninstall.sh
@@ -2,10 +2,10 @@ $MAGISK || { for FILE in ${POLS}; do
   if [ ! -z $XML ]; then
     [ "$(basename $FILE)" != "audio_policy_configuration.xml" ] && continue
     cp_ch $ORIGDIR$FILE $UNITY$FILE
-    sed -ri '/<mixPort name="(deep_buffer)|(raw)"/,/<\/mixPort>/ {s|( *)<!--(.*flags=".*".*)-->|\1\2|; /flags="AUDIO_OUTPUT_FLAG_FAST"/d}' $UNITY$FILE
+    sed -ri "/<mixPort name=\"(deep_buffer)|(raw)\"/,/<\/mixPort>/ {s|( *)<!--(.*flags=\".*\".*)$MODID-->|\1\2|; /flags=\"AUDIO_OUTPUT_FLAG_FAST\"/d}" $UNITY$FILE
   else
     cp_ch $ORIGDIR$FILE $UNITY$FILE
-    sed -i "/^# *raw {/,/}/ s/^#//" $UNITY$FILE
-    sed -i "/^# *deep_buffer {/,/}/ s/^#//" $UNITY$FILE
+    sed -i "/^# *raw {/,/}/ s/^#$MODID//" $UNITY$FILE
+    sed -i "/^# *deep_buffer {/,/}/ s/^#$MODID//" $UNITY$FILE
   fi
 done }
